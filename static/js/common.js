453122d02198dd755e46a2a5ae70116244a0944a
오하늘 <>
2024-01-19 17:42:31 +0900

    ui 만들기

--- static/js/common.js
+++ static/js/common.js
@@ -14,3 +14,24 @@ lenisScroll.on("scroll", event => {
       $(header).removeClass("scroll")
     }
 })
+
+$(".select-box").click(function(e)  {
+  if (e.currentTarget !== e.target) return;
+  const top = e.target.offsetTop + e.target.clientHeight + 12
+  $(".select-box-options").toggleClass("active")
+  $(".select-box-options").css("top", `${top}px`)
+})
+
+$(".select-box-option").click(function (e) {
+  const value = $(e.currentTarget).text()
+  $(".select-box span").text(value)
+  $(".select-box-options").removeClass("active")
+})
+
+$("body").click(function(e) {
+  if ($(".select-box-options").hasClass("active")) {
+    if (!$(e.target).is(".select-box-options, .select-box-option")) {
+      $(".select-box-options").removeClass("active");
+    }
+  }
+});
